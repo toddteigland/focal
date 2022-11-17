2629351feacf2e9dff90d09bb476e4591ef5a013
toddteigland <toddteigland@gmail.com>
2022-11-17 14:33:04 -0800

    Added folder, then a file called maptest to work with the built in function called map

---
+++ functions/map_builtin/mapTest.js
@@ -0,0 +1,5 @@
+const lighthouses = ["Gibraltar Point", "Peggy's Point", "Cove Island", "Discovery Island", "Cape Scott", "Point Clark", "Kincardine"];
+
+const Array = lighthouses.map(lighthouse => lighthouse.length);
+
+console.log(Array);
