5f714756c78e6619623b7d19ddcd1603175992ea
toddteigland <toddteigland@gmail.com>
2022-11-21 09:31:29 -0800

    trying  to solved it without loops. No success so far

--- pig-latin.js
+++ pig-latin.js
@@ -1,24 +1,34 @@
 const inputs = process.argv;
 
-let latinIt = function(inputs) {
-  let newArr = [];
-  newArr = inputs.slice(2);
-  for (let i = 0; i < newArr.length; i++) {
+// let latinIt = function(inputs) {
+//   let newArr = [];
+//   newArr = inputs.slice(2);
+//   for (let i = 0; i < newArr.length; i++) {
 
-    for (let j = 0; j < newArr[j].length; j++) {
-      let firstWord = [];
-      firstWord = newArr[j];
-      let firstChar = [];
-      firstChar = firstWord[0];
-      let unshiftedWord = [];
-      unshiftedWord = firstWord.unshift([0]);
-      let newWord = unshiftedWord.push(firstChar) + "ay";
-      console.log(firstWord[0]);
+//     for (let j = 0; j < newArr[j].length; j++) {
+//       let firstWord = [];
+//       firstWord = newArr[j];
+//       let firstChar = [];
+//       firstChar = firstWord[0];
+//       let unshiftedWord = [];
+//       //unshiftedWord = firstWord.unshift([0]);
+//       let newWord = unshiftedWord.push(firstChar) + "ay";
+//       console.log(firstWord[0]);
 
-    } 
+//     } 
 
+//   }
+// };
+// latinIt([0,1,"hello","world"]);
+
+let piggy = function(words) {
+  let latinWords = [];
+  let array = words.split(' ');
+  for (let word of array) {
+    latinwords = word[0].slice().pop;
+    
   }
-};
+  return latinWords.slice();
+}
 
-
-latinIt([0,1,"hello","world"]);
+console.log(piggy("Hello"));
